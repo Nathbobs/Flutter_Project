6019163bd2152418a08b5b0070ecff60a9c270bf
Nathbobs <46452812+Nathbobs@users.noreply.github.com>
2024-05-18 01:06:37 +0900

    PET DETAILS PAGE 2

--- lib/pet_det2.dart
+++ lib/pet_det2.dart
@@ -11,7 +11,7 @@ class _pet_det2State extends State<pet_det2> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.pink[100],
+      backgroundColor: Color(0xFFFFD9E5),
       appBar: AppBar(
         title: Text('가입하기'),
         backgroundColor: Colors.transparent,
@@ -24,12 +24,14 @@ class _pet_det2State extends State<pet_det2> {
         ),
       ),
       body: Padding(
-        padding: EdgeInsets.all(16.0),
+        padding: EdgeInsets.all(76.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('성별'),
-            SizedBox(height: 16),
+            Text(
+                '성별', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
             Row(
               children: [
                 Expanded(
@@ -37,12 +39,12 @@ class _pet_det2State extends State<pet_det2> {
                     onPressed: () {
                       // TODO: Handle male selection
                     },
-                    child: Text('공주님'),
+                    child: Text('공주님', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.white,
                       foregroundColor: Colors.black,
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
+                        borderRadius: BorderRadius.circular(42),
                       ),
                     ),
                   ),
@@ -53,12 +55,12 @@ class _pet_det2State extends State<pet_det2> {
                     onPressed: () {
                       // TODO: Handle female selection
                     },
-                    child: Text('왕자님'),
+                    child: Text('왕자님' , style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.white,
                       foregroundColor: Colors.black,
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
+                        borderRadius: BorderRadius.circular(42),
                       ),
                     ),
                   ),
@@ -66,49 +68,127 @@ class _pet_det2State extends State<pet_det2> {
               ],
             ),
             SizedBox(height: 32),
-            Text('종'),
+            Text(
+              '종', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
             SizedBox(height: 16),
-            TextFormField(
+          Container(
+            // width: 450.0, // specify your desired width
+            // height: 48.0, // specify your desired height
+            decoration: BoxDecoration(
+              color: Colors.white, // specify your desired background color
+              borderRadius: BorderRadius.circular(42), // specify your desired border radius
+            ),
+            child: TextFormField(
               decoration: InputDecoration(
-                hintText: '종을 선택해주세요',
-                border: OutlineInputBorder(),
+                labelText: '종을 선택해주세요',
+                labelStyle: TextStyle(color: Color(0xFFBABABA)),
+                contentPadding: EdgeInsets.only(left: 20),
+                border: InputBorder.none,
               ),
+            ),
             ),
             SizedBox(height: 32),
-            Text('생일'),
-            SizedBox(height: 16),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: 'YYMMDD',
-                border: OutlineInputBorder(),
-              ),
+            Text(
+              '생일', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 32),
-            Text('체중 (kg)'),
             SizedBox(height: 16),
-            TextFormField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-              ),
-              keyboardType: TextInputType.number,
-            ),
-            Spacer(),
-            Container(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  // TODO: Implement next step logic
-                },
-                child: Text('다음'),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.black,
-                  padding: EdgeInsets.symmetric(vertical: 16),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(32),
+                    ),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'YY',
+                        labelStyle: TextStyle(color: Color(0xFFBABABA)),
+                        contentPadding: EdgeInsets.only(left: 20),
+                        border: InputBorder.none,
+                      ),
+                    ),
                   ),
+                ),
+                SizedBox(width: 10), // Add some space between the boxes
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(32),
+                    ),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'MM',
+                        labelStyle: TextStyle(color: Color(0xFFBABABA)),
+                        contentPadding: EdgeInsets.only(left: 20),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10), // Add some space between the boxes
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(32),
+                    ),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'DD',
+                        labelStyle: TextStyle(color: Color(0xFFBABABA)),
+                        contentPadding: EdgeInsets.only(left: 20),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 32),
+            Text(
+              '체중 (kg)', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+
+            SizedBox(height: 16),
+            Container(
+              // width: 450.0, // specify your desired width
+              // height: 48.0, // specify your desired height
+              decoration: BoxDecoration(
+                color: Colors.white, // specify your desired background color
+                borderRadius: BorderRadius.circular(42), // specify your desired border radius
+              ),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  // labelText: '종을 선택해주세요',
+                  // labelStyle: TextStyle(color: Color(0xFFBABABA)),
+                  border: InputBorder.none,
                 ),
               ),
             ),
+            Spacer(),
+            Align(
+              alignment: Alignment.centerRight,
+              child: Container(
+                width: 70, // specify your desired width
+                child: ElevatedButton(
+                  onPressed: () {
+                    // TODO: Handle 완료 button press
+                  },
+                  child: Text('완료'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    foregroundColor: Colors.white,
+                    padding: EdgeInsets.symmetric(vertical: 16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
